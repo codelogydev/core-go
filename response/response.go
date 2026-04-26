48475b57e99d118d0527656984c0f4e5cc0400ee
danixsofyan <danixsofyan@gmail.com>
2026-04-26 13:21:46 +0700

    feat: add error response helper

--- response/response.go
+++ response/response.go
@@ -5,6 +5,13 @@ import "github.com/gin-gonic/gin"
 func Success(c *gin.Context, data interface{}) {
 	c.JSON(200, gin.H{
 		"success": true,
-		"data": data,
+		"data":    data,
+	})
+}
+
+func Error(c *gin.Context, code int, message string) {
+	c.JSON(code, gin.H{
+		"success": false,
+		"error":   message,
 	})
 }
